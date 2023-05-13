cfc5abbc13c8d6891e49030a047df79cad949a29
Drew Bidmead <dmbidmead@gmail.com>
2023-05-13 14:01:51 -0400

    Sidebar styled and functional

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
+    sidebar.classList.toggle('visible');
 })
